ed887c394928d5d4ecc47276a2a4152e715f7029
yajuvendrarawat <43192654+yajuvendrarawat@users.noreply.github.com>
2024-05-09 15:39:16 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,6 @@
 import os
-
 # from dotenv import load_dotenv
 from chromadb.config import Settings
-
 # https://python.langchain.com/en/latest/modules/indexes/document_loaders/examples/excel.html?highlight=xlsx#microsoft-excel
 from langchain.document_loaders import CSVLoader, PDFMinerLoader, TextLoader, UnstructuredExcelLoader, Docx2txtLoader
 
@@ -11,9 +9,7 @@ ROOT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 
 # Define the folder for storing database
 SOURCE_DIRECTORY = f"{ROOT_DIRECTORY}/SOURCE_DOCUMENTS"
-
 PERSIST_DIRECTORY = f"{ROOT_DIRECTORY}/DB"
-
 # Can be changed to a specific number
 INGEST_THREADS = os.cpu_count() or 8
 
@@ -22,7 +18,6 @@ CHROMA_SETTINGS = Settings(
     anonymized_telemetry=False,
     is_persistent=True,
 )
-
 
 # https://python.langchain.com/en/latest/_modules/langchain/document_loaders/excel.html#UnstructuredExcelLoader
 DOCUMENT_MAP = {
